dbce193c06345de361882fcc655ec435b27bf22e
ljch <ljch0403@163.com>
2018-07-31 19:06:45 +0800

    添加listview和imageLoader的jar包

--- app/src/main/java/com/ljch/helloworld/MainActivityFragment.kt
+++ app/src/main/java/com/ljch/helloworld/MainActivityFragment.kt
@@ -1,18 +1,30 @@
 package com.ljch.helloworld
 
-import android.support.v4.app.Fragment
+import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 
+import kotlinx.android.synthetic.main.fragment_main.*
 /**
  * A placeholder fragment containing a simple view.
  */
 class MainActivityFragment : Fragment() {
 
+    val data:Array<String> =  arrayOf("imageLoader", "glide")
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val myAdapter : ArrayAdapter<String> = ArrayAdapter(context,android.R.layout.simple_list_item_1,data)
+        list_view.adapter = myAdapter
+    }
+
+
 }
