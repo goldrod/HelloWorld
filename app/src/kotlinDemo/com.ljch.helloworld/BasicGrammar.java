4859465fe6cd9d1f01a0087d8eeecb4f0fd7116d
ljch <ljch0403@163.com>
2018-08-06 09:44:10 +0800

    kotlin语法学习

---
+++ app/src/kotlinDemo/com.ljch.helloworld/BasicGrammar.java
@@ -0,0 +1,17 @@
+//包定义
+package com.ljch.helloword
+
+open java.lang.Object
+
+/**
+ * kotlin的基础语法学习
+ **/
+class BasicGrammar {
+
+    //定义函数
+    fun sum(a:Int,b:Int):Int{
+        return a + b
+    }
+
+
+}
--- app/src/main/java/com/ljch/helloworld/MainActivityFragment.kt
+++ app/src/main/java/com/ljch/helloworld/MainActivityFragment.kt
@@ -2,9 +2,11 @@ package com.ljch.helloworld
 
 import android.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -24,7 +26,12 @@ class MainActivityFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val myAdapter : ArrayAdapter<String> = ArrayAdapter(context,android.R.layout.simple_list_item_1,data)
         list_view.adapter = myAdapter
+        list_view.onItemClickListener = AdapterView.OnItemClickListener{parent, view, position, id -> goToPage(position) }
     }
 
+    //点击跳转页面
+    private fun goToPage(position:Int){
+        Log.i("MainActivityFragment", "position = $position")
+    }
 
 }
